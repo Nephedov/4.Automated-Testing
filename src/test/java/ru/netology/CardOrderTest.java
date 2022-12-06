b6df8751571a7202abaf729be952cd0a3cd38893
Mihail Nephedov <Nephedov93@gmail.com>
2022-12-06 23:23:59 +0300

    edit CardOrderTest.class(19), add WebDriverManager.chromedriver().setup();

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,7 +16,7 @@ public class CardOrderTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
 
     }
 
